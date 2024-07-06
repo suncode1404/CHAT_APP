91716bb4fb26b5c44d6a6a39fb55e942f3ae402f
Thi Tinh <thitrungtinh@gmail.com>
2024-07-06 12:05:32 +0700

    connect mongodb + create controller,model-> user

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,11 +1,47 @@
-export const signup = (req,res) => {
-    console.log('signup');
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+    try {
+        const { fullname, username, password, confirmPassword, gender } = req.body;
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({ error: "Passwords don't match" });
+        }
+
+        const user = await User.findOne({ username })
+
+        //HASH PASSWORD HERE
+
+        //https://avatar-placeholder.iran.liara.run/
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        const newUser = new User({
+            fullname,
+            username,
+            password,
+            gender,
+            profielPic: gender == "male" ? boyProfilePic : girlProfilePic
+        })
+
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullname: newUser.fullname,
+            username: newUser.username,
+            profielPic: newUser.profielPic,
+        })
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({error:"Internal Server Error"})
+    }
 }
 
-export const login = (req,res) => {
+export const login = (req, res) => {
     console.log('login');
 }
 
-export const logout = (req,res) => {
+export const logout = (req, res) => {
     console.log('logout');
 }
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from '../backend/router/auth.routes.js'
+import conectToMonogoDB from './db/connectToMongoDB.js'
 const PORT = process.env.PORT || 8000
 
 const app = express()
 
 dotenv.config()
-app.get("/", (req, res) => {
-    //root route http://localhost:8000
-    res.send('Hello World!')
-})
+
+app.use(express.json()); //to parse the incoming requests with JSON payloads (from req.body)
 
 app.use("/api/auth/", authRoutes)
-app.listen(PORT, () => console.log(`Server Running http://localhost:${PORT}`))
+
+// app.get("/", (req, res) => {
+//     //root route http://localhost:8000
+//     res.send('Hello World!')
+// })
+
+app.listen(PORT, () => {
+    conectToMonogoDB()
+    console.log(`Server Running http://localhost:${PORT}`)
+})
